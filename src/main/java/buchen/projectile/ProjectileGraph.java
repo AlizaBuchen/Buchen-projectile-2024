39cb7e6f8717fd8021695ca2d688f2d3827df718
Aliza Buchen <alizabuchen@alizas-air-2.lan>
2024-02-27 17:08:07 -0500

    fixed homework

--- src/main/java/buchen/projectile/ProjectileGraph.java
+++ src/main/java/buchen/projectile/ProjectileGraph.java
@@ -3,12 +3,13 @@ package buchen.projectile;
 import javax.swing.*;
 import java.awt.*;
 
+
 public class ProjectileGraph extends JComponent
 {
     Projectile projectile = new Projectile(0, 0);
 
-    @Override
 
+    @Override
     protected void paintComponent(Graphics g)
     {
         super.paintComponent(g);
@@ -17,7 +18,7 @@ public class ProjectileGraph extends JComponent
 
         for (int t = 0; t <= projectile.getApexTime() * 2; t++)
         {
-            int currentX = (int) projectile.getX()
+            int currentX = (int) projectile.getX();
             int currentY = (int) projectile.getY();
             projectile.setSeconds(t);
             g.drawLine(currentX, -currentY, (int) projectile.getX(), -(int) projectile.getY());
@@ -25,6 +26,7 @@ public class ProjectileGraph extends JComponent
         g.setColor(Color.BLUE);
         g.fillOval((int)projectile.getInterceptX() / 2, (int) -projectile.getPeakY(), 15, 15);
     }
+
 
     public void setProjectile(Projectile projectile)
     {
