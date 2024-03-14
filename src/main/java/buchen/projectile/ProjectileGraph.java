d89669a21d7e4f097ac5092152176f0b0f1abbe3
Aliza Buchen <alizabuchen@Alizas-MacBook-Air-2.local>
2024-03-13 20:28:37 -0400

    test fixes

--- src/main/java/buchen/projectile/ProjectileGraph.java
+++ src/main/java/buchen/projectile/ProjectileGraph.java
@@ -8,7 +8,7 @@ import java.text.DecimalFormat;
 public class ProjectileGraph extends JComponent
 {
     Projectile projectile = new Projectile(0, 0);
-    Projectile projectileCopy = projectile;
+
 
     private static final DecimalFormat FORMAT = new DecimalFormat("0.00");
     @Override
@@ -36,6 +36,7 @@ public class ProjectileGraph extends JComponent
         g.drawLine(0, 0, getWidth(), 0);
         g.drawLine(0, 0, 0, -(getHeight()));
 
+        Projectile projectileCopy = new Projectile(projectile);
         projectile.setSeconds(0);
 
         for (int t = 0; t <= projectile.getApexTime() * 2; t++)
@@ -46,19 +47,18 @@ public class ProjectileGraph extends JComponent
             g.drawLine(currentX, -currentY, (int) projectile.getX(), -(int) projectile.getY());
         }
 
-        String locationCenter = "(" + FORMAT.format(projectile.getInterceptX() / 2) + ", " +
-                FORMAT.format(projectile.getPeakY()) + ")";
+        String locationCenter = "(" + FORMAT.format(projectile.getInterceptX() / 2) + ", "
+                + FORMAT.format(projectile.getPeakY()) + ")";
         g.setColor(Color.BLUE);
         g.drawString(locationCenter, (int) (projectile.getInterceptX() / 2), (int) -projectile.getPeakY());
         g.fillOval((int) projectile.getInterceptX() / 2 - 5, (int) -projectile.getPeakY() - 5, 10, 10);
 
-        String locationSeconds = "(" + FORMAT.format(projectile.getX()) + ", " +
-                FORMAT.format(projectile.getY()) + ")";
+
+        String locationSeconds = "(" + FORMAT.format(projectileCopy.getX()) + ", "
+                + FORMAT.format(projectileCopy.getY()) + ")";
         g.setColor(Color.RED);
 
-        projectile.setSeconds(0);
-
-        g.drawString(locationSeconds, (int) (projectileCopy.getX()), (int) -projectileCopy.getY());
+        g.drawString(locationSeconds, (int) (projectileCopy.getX()) -30, (int) -projectileCopy.getY() -30);
         g.fillOval((int) projectileCopy.getX() - 5, (int) -projectileCopy.getY() - 5, 10, 10);
     }
 
